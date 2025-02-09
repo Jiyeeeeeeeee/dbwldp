32d26803c0f05171a5bdac047f9a605183e78d01
Jiyeeeeeeeee <108465292+Jiyeeeeeeeee@users.noreply.github.com>
2025-02-09 12:00:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const colors = ["#FFcc99", "#ff8200", "#b4b4b4","#f0b469", "#FFe3e3", "#FFffcc", "#5ac8c8"];
+const colors = ["#d11a1a", "#f0f0f0", "#b4b4b4", "#929292","#886754", "#fffd98","#FFffcc"];
 
 function changeBackground() {
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
