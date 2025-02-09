3713880e868b8b61333957a2716587f338c119b3
Jiyeeeeeeeee <108465292+Jiyeeeeeeeee@users.noreply.github.com>
2025-02-09 11:30:02 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const colors = ["#FFcc99", "#ff8200", "#b4b4b4","#f0b469", "#FFe3e3", "#FFffcc", "#5ac8c8"];
-
-function changeBackground() {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-}
-
-window.onload = changeBackground; 
