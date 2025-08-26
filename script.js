f879dd42992b739780359852627503beb3f4d845
유지예 <yujiye@yujiyeui-MacBookAir.local>
2025-08-26 23:25:42 +0900

    Apply local changes

--- script.js
+++
@@ -1,8 +0,0 @@
-const colors = ["#f1f1f1"];
-
-function changeBackground() {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-}
-
-window.onload = changeBackground; 
