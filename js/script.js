881c1c1e6bd12d4caaa4e036ef957b9d552888a8
유지예 <yoojiye@yujiyeui-MacBookPro.local>
2026-09-19 20:19:06 +0900

    0919-3

--- js/script.js
+++ js/script.js
@@ -1,33 +1,4 @@
-const underconMedia = [
-  "images-2/undercon-1.jpg",
-  "images-2/undercon-2.jpg",
-  "images-2/undercon-3.jpg",
-  "images-2/undercon-4.mp4"
-];
 
-const randomMedia =
-  underconMedia[Math.floor(Math.random() * underconMedia.length)];
-
-const container = document.getElementById("undercon-container");
-
-if (randomMedia.endsWith(".mp4")) {
-  const video = document.createElement("video");
-
-  video.src = randomMedia;
-  video.autoplay = true;
-  video.loop = true;
-  video.muted = true;
-  video.playsInline = true;
-
-  container.appendChild(video);
-} else {
-  const img = document.createElement("img");
-
-  img.src = randomMedia;
-  img.alt = "Under construction";
-
-  container.appendChild(img);
-}
 // const projects = [
 //   {
 //     id: "project-16",
